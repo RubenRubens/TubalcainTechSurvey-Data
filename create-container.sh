01925dcbf282da81c3d2d6b61d833af91de14af1
Ruben Arriazu <ruben.arsi@protonmail.ch>
2022-02-09 18:00:11 +0100

    Cambia de podman a docker

--- create-container.sh
+++ create-container.sh
@@ -1,7 +1,6 @@
-podman run -ti \
-    --name basexhttp-exp \
+docker run -d \
+    --name basexhttp \
     --publish 1984:1984 \
     --publish 8984:8984 \
-    --volume $(pwd)/basex:/srv:Z \
-    -d basex/basexhttp:latest
-
+    --volume $(pwd)/basex:/srv/basex \
+    basex/basexhttp:latest
